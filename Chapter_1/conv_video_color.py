84513c789dcf525d5e3f0726a235fc31d2520917
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-08-12 15:44:05 +0000

    Converting video color

---
+++ Chapter_1/conv_video_color.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- Chapter_1/intro_videos.py
+++ Chapter_1/intro_videos.py
@@ -1,6 +1,8 @@
 import cv2
 
 # cap = cv2.VideoCapture("./my_videos/office.mp4")
+
+"""
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -15,4 +17,26 @@ while True:
         break
 
 cap.release()
+cv2.destroyAllWindows()
+
+"""
+
+# Write Videos
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter.fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", fourcc, 25, (640, 480))
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    out.write(frame)
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+out.release()
 cv2.destroyAllWindows()
